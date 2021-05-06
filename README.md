49ca8b2599964096194d891140cf92c084323d17
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-05-06 21:31:33 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Who is in Chime
+
+Used this repository.
+https://github.com/JCMais/hubot-typescript-boilerplate
---
+++ src/scripts/example.ts
@@ -0,0 +1,57 @@
+// Description:
+//   <description of the scripts functionality>
+//
+// Dependencies:
+//   "<module name>": "<module version>"
+//
+// Configuration:
+//   LIST_OF_ENV_VARS_TO_SET
+//
+// Commands:
+//   hubot <trigger> - <what the respond trigger does>
+//   <trigger> - <what the hear trigger does>
+//
+// Notes:
+//   <optional notes required for the script>
+//
+// Author:
+//   JCMais
+import axios from 'axios'
+import { Response, Robot } from 'hubot'
+import { SlackAdapter } from 'hubot-slack'
+
+import {
+  assertNotInsideThread,
+  assertNotPrivateMessage,
+  assertWorkInProgress,
+} from '../utils'
+
+module.exports = async function example(robot: Robot<SlackAdapter>) {
+  robot.respond(
+    /(http(?:s?):\/\/(\S*))/i,
+    async (res: Response<SlackAdapter>) => {
+      const url = res.match[1]
+      res.send(`ok1: ${url}`)
+
+      await axios.get(url)
+
+      res.send(`ok2: ${url}`)
+    },
+  )
+
+  robot.respond(/test private$/, async (res: Response<SlackAdapter>) => {
+    await assertNotPrivateMessage(robot, res)
+  })
+
+  robot.respond(/test thread$/, async (res: Response<SlackAdapter>) => {
+    await assertNotInsideThread(robot, res)
+  })
+
+  robot.respond(/test wip$/, async (res: Response<SlackAdapter>) => {
+    await assertWorkInProgress(robot, res)
+  })
+
+  robot.respond(/test error$/, async (res: Response<SlackAdapter>) => {
+    throw new Error('Are uncaught errors being sent to channel?')
+  })
+}
