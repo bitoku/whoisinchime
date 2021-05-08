fcd7937e6d075e6ac922e7f9a66a3a66218ca0ca
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-05-08 21:24:41 +0100

    show display name

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -2,10 +2,16 @@ import AWS from 'aws-sdk';
 import { Response, Robot } from 'hubot'
 import { SlackAdapter } from 'hubot-slack'
 
+const accountId = process.env.ACCOUNT_ID;
+if (!accountId) {
+  throw new Error("No account id");
+}
+
 module.exports = async function main(robot: Robot<SlackAdapter>) {
-  robot.respond(/^who is in (\W)$/, async (res: Response<SlackAdapter>) => {
+  robot.respond(/who is in ([a-fA-F0-9]{8}(?:-[a-fA-F0-9]{4}){3}-[a-fA-F0-9]{12})$/, async (res: Response<SlackAdapter>) => {
     const meetingId = res.match[1];
-    const chime = new AWS.Chime({region: 'eu-west-1'});
+    const chime = new AWS.Chime({region: 'us-east-1'});
+
     const response = await chime.listAttendees({
       MeetingId: meetingId,
     }).promise();
@@ -17,6 +23,17 @@ module.exports = async function main(robot: Robot<SlackAdapter>) {
       res.send("There are no attendees.");
       return;
     }
-    res.send(response.Attendees.map(attendee => `@${attendee.AttendeeId}`).join(", "));
+
+    const users = await Promise.all(response.Attendees.map(async attendee => {
+      if (!attendee.AttendeeId) return "Unknown";
+      const user = await chime.getUser({
+        AccountId: accountId,
+        UserId: attendee.AttendeeId
+      }).promise();
+      if (!user) return "Unknown";
+      if (!user.User) return "Unknown";
+      return user.User.DisplayName || user.User.UserId;
+    }));
+    res.send(users.join(", "));
   })
 }
